124df108142208beafa0180b6852aeb07a423a2e
headblade-dev <hix6.yt@gmail.com>
2026-09-24 21:13:18 +0400

    Corrected luminance calculation (added 0.0722 coefficient to blue channel)

--- crates/aula-app/src/board.rs
+++ crates/aula-app/src/board.rs
@@ -85,7 +85,7 @@ pub fn draw_board(ui: &egui::Ui, rect: egui::Rect, layout: &[KeyPos], frame: &Fr
         let key_text_color = if c.is_black() {
             pal.text_muted // Set dimmed text on disabled LEDs
         } else { // Else get LED luminance (from Rec.709 TV standard formula) and compare with contrast threshold (150)
-            let lum = 0.2126 * c.r as f32 + 0.7152 * c.g as f32 + c.b as f32;
+            let lum = 0.2126 * c.r as f32 + 0.7152 * c.g as f32 + 0.0722 * c.b as f32;
             if lum > 160.0 {
                 egui::Color32::BLACK
             } else {
